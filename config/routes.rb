b289b409fbddbf6855a317a194bcc37ba217ea45
bobanderic2000 <bobanderic2000@gmail.com>
2020-12-31 14:01:46 +0800

    g scaffold Message; migrate all migrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  resources :messages
   resources :rooms
+
+
   get '/playgrounds/:id' => 'playgrounds#show'
   # resources playgrounds, only: :show
   root 'pages#index'
   get 'pages/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
