4ffb7d908757af63664130f2d9938efd0c3b8ff9
bobanderic2000 <bobanderic2000@gmail.com>
2020-12-31 14:39:48 +0800

    add bootstrap, rooms index

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   resources :messages
   resources :rooms
-
+  root 'rooms#index'
 
   get '/playgrounds/:id' => 'playgrounds#show'
   # resources playgrounds, only: :show
-  root 'pages#index'
   get 'pages/index'
 end
