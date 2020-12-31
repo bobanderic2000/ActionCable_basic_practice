593e0f928ad60ddf67ae4a4ea859c15febd338ce
bobanderic2000 <bobanderic2000@gmail.com>
2020-12-31 16:12:12 +0800

    able to create msgs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
   validates :username, uniqueness: true
+
+  def self.generate
+    adjectives = ['Ancient', 'Creative', 'Dangerous', 'Effective', 'Flying']
+    nouns = ['Highway', 'Intern', 'Jackhammer', 'Lion']
+    number = rand.to_s[2..4]
+    username = "#{adjectives.sample}-#{nouns.sample}-#{number}"
+    create(username: username)
+  end
 end
